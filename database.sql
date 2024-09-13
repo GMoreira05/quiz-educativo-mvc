8087c4d233ea8f3e3b2c370b5072626c1682c61f
GMoreira05 <henrique_gabriel11@outlook.com>
2024-09-13 16:22:37 -0300

    racunho bd

---
+++ database.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE IF NOT EXISTS db_jogo_quiz;
+USE db_jogo_quiz;
+
+CREATE TABLE IF NOT EXISTS administradores (
+    id INT NOT NULL AUTO_INCREMENT,
+    usuario VARCHAR(16) NOT NULL UNIQUE,
+    senha VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS questoes (
+    id INT NOT NULL AUTO_INCREMENT,
+    enunciado TEXT NOT NULL,
+    alternativa_correta CHAR(1) NOT NULL,
+    alternativa_a TEXT NOT NULL,
+    alternativa_b TEXT NOT NULL,
+    alternativa_c TEXT NOT NULL,
+    alternativa_d TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS partidas (
+    id INT NOT NULL AUTO_INCREMENT,
+    pontuacao INT NOT NULL DEFAULT(0),
+    finalizada BOOL NOT NULL DEFAULT(false)
+);
+
+CREATE TABLE IF NOT EXISTS partidas_questoes (
+    id INT NOT NULL AUTO_INCREMENT,
+    id_partida INT NOT NULL,
+    id_questao INT NOT NULL,
+    resposta escolhida CHAR(1)
+);
